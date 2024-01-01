48135eeef917bdf66d48488138c2c269c8ee99c1
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-01-02 00:07:51 +0900

    Test1

--- src/main/java/umc/spring/umcspring/ApiPayload/Code/Status/ErrorStatus.java
+++ src/main/java/umc/spring/umcspring/ApiPayload/Code/Status/ErrorStatus.java
@@ -27,7 +27,7 @@ public enum ErrorStatus implements BaseErrorCode {
 
     // 미션 관련 에러
     MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "MISSION40001", "미션이 없습니다."),
-    MISSION_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "MISSION40002", "이미 미션을 수락했습니다."),
+    MISSION_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "MISSION40002", "이미 미션을 수락."),
 
     // 예시
     ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE40001", "게시글이 없습니다."),
